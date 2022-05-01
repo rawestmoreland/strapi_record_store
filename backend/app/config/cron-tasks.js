2dc50fb8a9311576b045987bd3b92909c1026d9e
Richard Westmoreland <kegtrack@gmail.com>
2022-05-01 12:02:07 -0300

    cron

--- backend/app/config/cron-tasks.js
+++ backend/app/config/cron-tasks.js
@@ -8,7 +8,7 @@ module.exports = {
   /**
    * Every day at 3:30 AM
    */
-  "*/30 * * * *": async ({ strapi }) => {
+  "*/120 * * * *": async ({ strapi }) => {
     let NEW_INVENTORY_COUNT = 0;
     let OLD_INVENTORY_COUNT = 0;
     let REMOVE_COUNT = 0;
