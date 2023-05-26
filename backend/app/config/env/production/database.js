d9581ffb5e8052675fa989f8d1d8481d67d70c30
Richard Westmoreland <richard.westmoreland@scottscheapflights.com>
2023-05-26 12:35:44 -0500

    database prod file

--- backend/app/config/env/production/database.js
+++ backend/app/config/env/production/database.js
@@ -1,19 +1,13 @@
-const parse = require("pg-connection-string").parse;
-const config = parse(
-  process.env.DATABASE_URL
-);
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false,
-      },
+      host: env('DATABASE_HOST', '0.0.0.0'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
+      ssl: env.bool('DATABASE_SSL', false),
     },
     debug: false,
   },
